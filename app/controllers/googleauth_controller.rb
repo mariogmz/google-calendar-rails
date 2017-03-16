a1939ceff2c5ebc5089c5152f6aad73abf785788
Mario Gomez <mariogomezmtz@gmail.com>
2017-03-16 13:34:49 -0600

    Add action for new events

--- app/controllers/googleauth_controller.rb
+++ app/controllers/googleauth_controller.rb
@@ -53,4 +53,26 @@ class GoogleauthController < ApplicationController
 
     @event_list = service.list_events(params[:calendar_id])
   end
+
+  def new_event
+    client = Signet::OAuth2::Client.new({
+      client_id: Rails.application.secrets.google_client_id,
+      client_secret: Rails.application.secrets.google_client_secret,
+      token_credential_uri: 'https:://accounts.google.com/o/oauth2/token'
+    })
+
+    client.update!(session[:authorization])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+
+    event = Google::Apis::CalendarV3::Event.new({
+      start: Google::Apis::CalendarV3::EventDateTime.new(date: Date.today),
+      end: Google::Apis::CalendarV3::EventDateTime.new(date: Date.today + 1),
+      summary: "New event made with Google Calendar Rails"
+    })
+
+    service.insert_event(params[:calendar_id], event)
+    redirect_to events_url(calendar_id: params[:calendar_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/callback', to: 'googleauth#callback', as: 'callback'
   get '/calendars', to: 'googleauth#calendars', as: 'calendars'
   get '/events/:calendar_id', to: 'googleauth#events', as: 'events', calendar_id: /[^\/]+/
+  post '/events/:calendar_id', to: 'googleauth#new_event', as: 'new_event', calendar_id: /[^\/]+/
 end
